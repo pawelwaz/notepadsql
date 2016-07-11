843f4c548540688dccbdab9787434502a654cdd2
pawelwaz <waz-pawel@wp.pl>
2016-07-11 11:16:30 +0200

    początek projektu

---
+++ src/notepadsql/Loader.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepadsql;
+
+/**
+ *
+ * @author A20111
+ */
+public class Loader {
+    public static void goTo(String layout) {
+        
+    }
+}
---
+++ src/notepadsql/LoginController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepadsql;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author A20111
+ */
+public class LoginController implements Initializable {
+    @FXML private AnchorPane ap;
+    
+    @FXML
+    private void logUser(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void goRegister() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+            Stage thisStage = (Stage) ap.getScene().getWindow();
+            thisStage.close();
+        }
+        catch(Exception e) {
+            
+        }
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
