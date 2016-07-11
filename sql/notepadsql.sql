7e4e20a30744be52cea90fedb0f68c87af1a2b34
pawelwaz <waz-pawel@wp.pl>
2016-07-11 11:38:14 +0200

    skrypt sql

---
+++ sql/notepadsql.sql
@@ -0,0 +1,32 @@
+create database if not exists notepadsql DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS 'notepadsql'@'%' IDENTIFIED BY 'notepadsql';
+grant all privileges on notepadsql.* to 'notepadsql'@'%';
+flush privileges;
+use notepadsql;
+
+create table if not exists users(
+	usersid int not null auto_increment,
+    login varchar(32) not null unique,
+    password varchar(32) not null,
+    primary key(usersid)
+);
+
+create table if not exists categories(
+	categoriesid int not null auto_increment,
+    name varchar(32) not null,
+    description text,
+    categories_usersid int not null,
+    foreign key (categories_usersid) references users(usersid),
+    primary key(categoriesid)
+);
+
+create table if not exists notes(
+	notesid int not null auto_increment,
+    title varchar(32) not null,
+    content text,
+    notes_categoriesid int not null,
+    notes_usersid int not null,
+    foreign key (notes_categoriesid) references categories(categoriesid),
+    foreign key (notes_usersid) references users(usersid),
+    primary key(notesid)
+);
